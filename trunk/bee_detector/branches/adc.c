3bdcdf21be41d62b6ed2702d2aa83c61921f395b
daniel.hajtas@gmail.com <daniel.hajtas@gmail.com@3bb3db53-28f8-4683-86d9-d27b18a445d0>
2013-01-30 21:49:12 +0000

    Added global Mask_xxx, scanning 2 channels at once

--- trunk/bee_detector/branches/adc.c
+++ trunk/bee_detector/branches/adc.c
@@ -19,7 +19,7 @@ volatile uint8_t ADC_CH0_PIN, ADC_CH1_PIN;
 
 //volatile int16_t ADC_Buffer[10];
 //volatile uint16_t ADC_Index;
-/*
+
 ISR(ADC_INT0)
 {
 //	const prog_int16_t *window = tbl_window;
@@ -40,7 +40,7 @@ ISR(ADC_INT0)
 	if(adc_deci == 4)
 	{
 		Signal[0][adc_i] >>= 1;		//Decimation by 2
-		Signal[0][adc_i] -= ADC_Cal[ADC_CH0_PIN];
+//		Signal[0][adc_i] -= ADC_Cal[ADC_CH0_PIN];
 		adc_i++;
 		adc_deci = 0;
 	}
@@ -49,11 +49,11 @@ ISR(ADC_INT0)
 	{
 		adc_i = 0;
 		ADC_Status |= ADC0_FINISHED;
-//		ADCA.CTRLB &= ~ADC_FREERUN_bm;									 		//disable freerunning... (if in freerunning mode)
+		ADCA.CTRLB &= ~ADC_FREERUN_bm;									 		//disable freerunning... (if in freerunning mode)
 		ADCA.CH0.INTCTRL = ADC_CH_INTMODE_COMPLETE_gc | ADC_CH_INTLVL_OFF_gc;		//0xFC;  	//disable interrupt from CH0
 	}
 }
-*/
+
 
 ISR(ADC_INT1)
 {
@@ -63,8 +63,8 @@ ISR(ADC_INT1)
 	int16_t v, vv;
 	//  urobit kalibraciu 0 ako jedno cele meranie a vypocitat priemernu hodnotu - ulozit do eeprom?
 	//	v = ADCA.CH0RES - 0x0874;	// 0V = 0x0C8... 1/2 je o cca 200dec vyssie ako 0x7FF
-	v = ADCA.CH0RES;
-	vv = ADCA.CH1RES;				// 0V = 0x0C8... 1/2 je o cca 200dec vyssie ako 0x7FF
+	v = ADCA.CH0RES - 0x0874;
+	vv = ADCA.CH1RES - 0x0874;				// 0V = 0x0C8... 1/2 je o cca 200dec vyssie ako 0x7FF
 	Signal[0][adc_i] += v;
 	Signal[1][adc_i] += vv;
 	//	PORTD.OUTTGL = _BV(LED0);		//sampling frequency test
@@ -114,23 +114,29 @@ void ADC_Init(void)
 	ADC1_PORT.DIR = 0x00; 						// configure a2d port (PORTF) as input so we can receive analog signals
 	PORTCFG.MPCMASK = 0xFF;						// copy settings to all pins
 	ADC0_PORT.PIN0CTRL = 0x07; 					// make sure pull-up resistors are turned off, digital input buffer disabled
-
+/*
 	for(ii=0;ii<12;ii++)						// clear zero point for all possible channels
 	{
 		ADC_Cal[ii] = 0;
 	}
-
-	ADCA.CTRLA = ADC_ENABLE_bm | ADC_FLUSH_bm;	// enable ADC, no DMA controll, no conversion start, flush pipeline 
-	ADCA.CTRLB = ADC_IMPMODE_LOWIMP_gc | ADC_CURRLIMIT_NO_gc | ADC_RESOLUTION_12BIT_gc;		// freerunning at this time?
-	ADCA.REFCTRL = ADC_REFSEL_VCC_gc | ADC_BANDGAP_bm;		// enable bandgap, reference = VCC/1.6
+*/
+	ADCA.CTRLA = ADC_ENABLE_bm | ADC_FLUSH_bm;	// enable ADC, no DMA control, no conversion start, flush pipeline 
+	ADCA.CTRLB = ADC_IMPMODE_LOWIMP_gc | ADC_CURRLIMIT_NO_gc | ADC_RESOLUTION_12BIT_gc;		// free-running at this time?
+	ADCA.REFCTRL = ADC_REFSEL_VCC_gc | ADC_BANDGAP_bm;		// enable band-gap, reference = VCC/1.6
 	ADCA.PRESCALER = ADC_PRESCALER_DIV256_gc;				// clk/512 - 31.25kSps @ 16MHz; clk/256 - 125kSps @ 32MHz, decimated by 2 - 13bit @ 31.25kSps
-	ADCA.EVCTRL = ADC_SWEEP_01_gc;							// freerunning channels 0 1
+	ADCA.EVCTRL = ADC_SWEEP_01_gc;							// free-running channels 0 1
+//	ADCA.EVCTRL = ADC_SWEEP_0_gc;							// free-running channel 0 
 //  ADCA.EVCTRL = 		//event control	
 //  ADCA.EVCTRL = 		//event control	
 //#if ADC_INT_ENABLE == 1
 	ADCA.CH0.CTRL = ADC_CH_INPUTMODE_SINGLEENDED_gc;	// selects single-ended conversion
 	ADCA.CH1.CTRL = ADC_CH_INPUTMODE_SINGLEENDED_gc;	// selects single-ended conversion
+
+
 	ADC_chswitch(0,1);
+//	ADCA.CH0.MUXCTRL = ADC_CH_MUXPOS_PIN0_gc;
+
+
 //	ADCA.CH0.INTCTRL = ADC_CH_INTMODE_COMPLETE_gc | ADC_CH_INTLVL_HI_gc;		// enable interrupt & free running mode
 // only interrupt on channel 1, channel 0 done 1 ADC clk before - given by design
 	ADCA.CH1.INTCTRL = ADC_CH_INTMODE_COMPLETE_gc | ADC_CH_INTLVL_HI_gc;		// enable interrupt & free running mode
@@ -154,6 +160,7 @@ void ADC_Init(void)
 
 
 	ADCA.CTRLB |= ADC_FREERUN_bm;							// start freerun - no start necessary???
+
 //	ADCA.CTRLA |= ADC_CH0START_bm || ADC_CH1START_bm;					// first conversion needs to be started, free running continues...
 //	ADCA.CH0.CTRL |= ADC_CH_START_bm;				// equivalent to previous one...
 	ADC_Status = 0;
