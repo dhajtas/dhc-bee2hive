3bc6692966922bc346e901ad5664c9a751b13818
daniel.hajtas@gmail.com <daniel.hajtas@gmail.com@3bb3db53-28f8-4683-86d9-d27b18a445d0>
2013-04-13 22:05:43 +0000

    Working write; 1wire, sht not working

--- trunk/bee_detector/branches/ds1820.c
+++ trunk/bee_detector/branches/ds1820.c
@@ -20,7 +20,7 @@ void ConvertT(uint8_t dallas)		//zaciatok merania pre DS1820
 uint8_t Read1820(uint8_t error)
 {
  uint8_t l, m, pokus=0;
- uint8_t errormask = 0x01;
+ uint8_t errormask = error;
 
 //???;*'vysledkom je 16 bytov v buffroch: 8x1.byte+8x2.byte...
 	do
@@ -28,7 +28,8 @@ uint8_t Read1820(uint8_t error)
 		owire(0xBE,error);		//read scratch, save it to 1Wbuff, read 9 bytes, switch off power after finish)
 		for(l = 0;l<8;l++)
 		{
-			ow_inp(OWbuff,l,error);	//zacinaj ukladat data vzdy od 1. dallasu byte "l"
+			//ow_inp(OWbuff,l,error);	//zacinaj ukladat data vzdy od 1. dallasu byte "l"
+			OWbuff[0].data[l] = ow_inp_1(error);
 		}
 		ow_rstprt(error);
 		ow_reset(error);
@@ -39,27 +40,27 @@ uint8_t Read1820(uint8_t error)
   	
   	if(error)		//ak bola chyba v comm, tak data pri chybe = 0xFF
   	{
-  		for(l=0;l<OW_NUM;l++)
-  		{
+//  		for(l=0;l<OW_NUM;l++)
+//  		{
   			if(error & errormask)
   			{
   				for(m=0;m<8;m++)
-  					OWbuff[l].data[m] = 0xFF;
+  					OWbuff[0].data[m] = 0xFF;
   			}
-  			errormask<<=1;	
-  		}
+//  			errormask<<=1;	
+//  		}
   	}
   	return(error);
 }
 //--------------------------------------------------------------
 
 
-uint16_t ReadTemp(uint8_t number)
+int16_t ReadTemp(uint8_t number)
 {
- register uint16_t pomw,rem = 0;
- register uint8_t count,pom;
+ register int16_t pomw; //,rem = 0;
+// register uint8_t count,pom;
  
-	pomw = (uint16_t)OWbuff[number].data[0];
+/*	pomw = (uint16_t)OWbuff[number].data[0];
 	pomw = pomw<<7;		//posun do horneho bytu, ale MSB bude znamienko a povodne LSB sa prepise (0.5 stupna)
 	if(!OWbuff[number].data[1])
 		pomw |= 0x8000;	//ak kladne tak 1, zaporne 0 (koli porovnavaniu velkosti v ramci unsigned!)	
@@ -77,6 +78,8 @@ uint16_t ReadTemp(uint8_t number)
 	}
 	else
 		pomw = 0xFFFF;		//ak bola chyba a data su 0xFF ta vysledok bude tiez 0xFFFF!
+*/
+	pomw = (uint16_t)OWbuff[number].data[1]<<8 | OWbuff[number].data[0]; 
 	return(pomw);
 }
 
